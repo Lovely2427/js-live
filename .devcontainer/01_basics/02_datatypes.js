e2d2ee757e7df07132e11b7e129053ce19f7676f
Lovely2427 <140387900+Lovely2427@users.noreply.github.com>
2024-08-31 13:08:45 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all js code as newer version
+
+//alert ( 3+3 ) // we are using node js, not browser
+
+console.log(3 + 3 ) //code readability should be high
+
+console.log("lovely")
+
+let name = "lovely"
+let age = 19
+let isLoggedIn = false
+
+//PRIMITIVE DATATYPES
+// number => 2 to power 53
+// bigint
+// string => ""
+//boolean => true, false 
+//null => standalone value (empty)
+//undefined => value not assigned
+//symbols => uniqueness
+
+//OBJECT
+
+console.log(typeof "lovely");
+
+console.log(typeof age )
+
+console.log(typeof null ) //null is object, keeping empty
+
+console.log(typeof undefined ) // undefined, datatype in itself, A variable that has not been assigned a value 
