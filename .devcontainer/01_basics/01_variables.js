45b0eb0720b6022341edc5e70407627fb0ce9d96
Lovely2427 <140387900+Lovely2427@users.noreply.github.com>
2024-08-31 12:38:35 +0000

    commit config done

--- .devcontainer/01_basics/01_variables.js
+++ .devcontainer/01_basics/01_variables.js
@@ -10,7 +10,8 @@ console.log(accountId);
 accountEmail = "prince@gmail.com"
 accountPassword = 34567
 accountCity = "Pune"
-
+/*perfer not to use var
+because of issue in block scope and functional scope */
 
 
 console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
