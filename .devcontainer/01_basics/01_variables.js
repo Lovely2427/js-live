6b6664695eeb7ce589d5742524188202c13359a2
Lovely2427 <140387900+Lovely2427@users.noreply.github.com>
2024-08-31 12:34:43 +0000

    commit config done

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 152
+let accountEmail = "lovelypandey2427@gmail.com"
+var accountPassword = "12345"
+accountCity = "delhi"
+let accountState;
+// accountId = 1 // not allowed
+
+console.log(accountId);
+
+accountEmail = "prince@gmail.com"
+accountPassword = 34567
+accountCity = "Pune"
+
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
