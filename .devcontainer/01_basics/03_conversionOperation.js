d029a0d7f7d3f5d76232051235115c5bc7d56cfd
Lovely2427 <140387900+Lovely2427@users.noreply.github.com>
2024-08-31 13:56:37 +0000

    commit config done

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+let score = 55 //number
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let age = "55" //string "" 
+
+console.log(typeof age );
+//console.log(typeof (age));
+ 
+// true => boolean
+
+ let valueInNumber = Number(age)
+ //console.log(typeof valueInNumber );
+ //console.log(valueInNumber);
+  
+ // "55" => 55
+ // "55abc" => Nan
+ //true => 1
+ //false => 0
+
+let isLoggedIn = 1
+let BooleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(BooleanIsLoggedIn);
+
+// 1 => true , 0 => false
+// "" => false
+// "lovely" => true
+
+let aNumber = 24
+let StringaNumber = String(aNumber)
+console.log(StringaNumber);
+console.log(typeof StringaNumber);
