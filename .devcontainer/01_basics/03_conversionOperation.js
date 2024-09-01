9d76fbd5b38ce693d99ee8d36a464bd54ced4add
Lovely2427 <140387900+Lovely2427@users.noreply.github.com>
2024-09-01 08:11:50 +0000

    commit config done

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = 55 //number
 
-console.log(typeof score);
-console.log(typeof (score));
+//console.log(typeof score);
+//console.log(typeof (score));
 
 let age = "55" //string "" 
 
-console.log(typeof age );
+//console.log(typeof age );
 //console.log(typeof (age));
  
 // true => boolean
@@ -29,5 +29,45 @@ let BooleanIsLoggedIn = Boolean(isLoggedIn)
 
 let aNumber = 24
 let StringaNumber = String(aNumber)
-console.log(StringaNumber);
-console.log(typeof StringaNumber);
+//console.log(StringaNumber);
+//console.log(typeof StringaNumber);
+
+
+// ************************* OPERATIONS **************************
+
+let value = 2
+let negValue = -value
+//console.log(negValue);
+
+//console.log(2+2)
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**2);
+//console.log(2/3);
+//console.log(2%3);
+
+let str1 = "lovely"
+let str2 = "ritu"
+
+let str3 = str1 + str2
+//console.log(str3);
+
+//console.log("1" + 2);
+//console.log(1 + "2");
+//console.log("1"+ 2 + 2);
+//console.log( 1 + 2 + "2");
+
+//console.log(+true);
+//console.log(+"");
+
+let num1 , num2 , num3
+
+num1 = num2 = num3 = 2+2 //bad practice
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
---
+++ .devcontainer/01_basics/04_comparison.js
@@ -0,0 +1,19 @@
+
+//console.log("2" > 1); 
+//console.log("02" > 1); dont compare different datatypes
+
+/*console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined < 0);
+console.log(undefined > 0);
+
+avoid this practice
+*/ 
+
+// === strict checking, it check datatype with number
+console.log("2" === 2);
+
+
